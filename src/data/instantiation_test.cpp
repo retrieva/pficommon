f863ea1d79af2f8686c8a94107a726a32a1f746e
pepshiso <pepshiso9@gmail.com>
2012-08-31 21:50:01 +0900

    Added instantiation test for data/string/ustring.h.

--- src/data/instantiation_test.cpp
+++ src/data/instantiation_test.cpp
@@ -14,3 +14,10 @@ template class pfi::data::string::kmp<std::string>;
 template int pfi::data::string::edit_distance<std::string>(const std::string&, const std::string&);
 template int pfi::data::string::longest_common_subsequence<std::string>(const std::string&, const std::string&);
 template int pfi::data::string::longest_increasing_subsequence<std::string>(const std::string&);
+
+
+#include "string/ustring.h"
+
+template pfi::data::string::ustring::ustring<char*>(char*, char*);
+template pfi::data::string::uchar pfi::data::string::chars_to_uchar<char*>(char*&);
+template void pfi::data::string::uchar_to_chars<char*>(uchar, char*&);
