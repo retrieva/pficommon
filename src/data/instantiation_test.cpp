23a32057c232ce5e9f21baf5aeb2fa12f512a788
pepshiso <pepshiso9@gmail.com>
2012-08-31 21:35:11 +0900

    Added instantiation tests for data/string/algorithm.h.

--- src/data/instantiation_test.cpp
+++ src/data/instantiation_test.cpp
@@ -7,3 +7,10 @@ template class pfi::data::fenwick_tree<int>;
 #include <string>
 
 template class pfi::data::string::kmp<std::string>;
+
+
+#include "string/algorithm.h"
+
+template int pfi::data::string::edit_distance<std::string>(const std::string&, const std::string&);
+template int pfi::data::string::longest_common_subsequence<std::string>(const std::string&, const std::string&);
+template int pfi::data::string::longest_increasing_subsequence<std::string>(const std::string&);
