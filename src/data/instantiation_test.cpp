6278229d2ed5e56109f1f255d3323a082613a34a
pepshiso <pepshiso9@gmail.com>
2012-08-31 21:55:36 +0900

    Added namespace alias in data/instantiation_tast.

--- src/data/instantiation_test.cpp
+++ src/data/instantiation_test.cpp
@@ -1,23 +1,27 @@
 #include "fenwick_tree.h"
 
-template class pfi::data::fenwick_tree<int>;
+namespace data = pfi::data;
+
+template class data::fenwick_tree<int>;
 
 
 #include "string/kmp.h"
 #include <string>
 
-template class pfi::data::string::kmp<std::string>;
+namespace string = data::string;
+
+template class string::kmp<std::string>;
 
 
 #include "string/algorithm.h"
 
-template int pfi::data::string::edit_distance<std::string>(const std::string&, const std::string&);
-template int pfi::data::string::longest_common_subsequence<std::string>(const std::string&, const std::string&);
-template int pfi::data::string::longest_increasing_subsequence<std::string>(const std::string&);
+template int string::edit_distance<std::string>(const std::string&, const std::string&);
+template int string::longest_common_subsequence<std::string>(const std::string&, const std::string&);
+template int string::longest_increasing_subsequence<std::string>(const std::string&);
 
 
 #include "string/ustring.h"
 
-template pfi::data::string::ustring::ustring<char*>(char*, char*);
-template pfi::data::string::uchar pfi::data::string::chars_to_uchar<char*>(char*&);
-template void pfi::data::string::uchar_to_chars<char*>(uchar, char*&);
+template string::ustring::ustring<char*>(char*, char*);
+template string::uchar pfi::data::string::chars_to_uchar<char*>(char*&);
+template void string::uchar_to_chars<char*>(uchar, char*&);
