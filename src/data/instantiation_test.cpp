cbc3312c056ab381b37e34d3d6cd3eaed93910fc
pepshiso <pepshiso9@gmail.com>
2012-09-03 06:40:36 +0900

    Added instantiation test for data/string/utility.h.

--- src/data/instantiation_test.cpp
+++ src/data/instantiation_test.cpp
@@ -2,7 +2,10 @@
 #include "string/kmp.h"
 #include "string/algorithm.h"
 #include "string/ustring.h"
+#include "string/utility.h"
+#include <deque>
 #include <string>
+#include <vector>
 
 namespace pfi {
 namespace data {
@@ -20,6 +23,27 @@ template int longest_increasing_subsequence<std::string>(const std::string&);
 template ustring::ustring<char*>(char*, char*);
 template uchar chars_to_uchar<char*>(char*&);
 template void uchar_to_chars<char*>(uchar, char*&);
+
+template void split<std::string, char, std::string*>(const std::string&, char, std::string*);
+template std::deque<std::string> split<std::deque<std::string>, std::string, char>(const std::string&, char);
+template std::vector<std::string> split<std::string, char>(const std::string&, char);
+template void split<std::string, std::string*>(const std::string&, const std::string&, std::string*);
+template std::deque<std::string> split<std::deque<std::string>, std::string>(const std::string&, const std::string&);
+template std::vector<std::string> split<std::string>(const std::string&, const std::string&);
+template void strip<std::string>(const std::string&, std::string&);
+template std::string strip<std::string>(const std::string&);
+template void lstrip<std::string>(const std::string&, std::string&);
+template std::string lstrip<std::string>(const std::string&);
+template void rstrip<std::string>(const std::string&, std::string&);
+template std::string rstrip<std::string>(const std::string&);
+template void join<std::string*, std::string>(std::string*, std::string*, const std::string&, std::string&);
+template void join<std::vector<std::string>, std::string>(const std::vector<std::string>&, const std::string&, std::string&);
+template std::string join<std::string*, std::string>(std::string*, std::string*, const std::string&);
+template std::string join<std::vector<std::string>, std::string>(const std::vector<std::string>&, const std::string&);
+template bool starts_with<std::string>(const std::string&, const std::string&);
+template bool ends_with<std::string>(const std::string&, const std::string&);
+template void replace<std::string>(const std::string&, const std::string&, const std::string&, std::string&);
+template std::string replace<std::string>(const std::string&, const std::string&, const std::string&);
 
 } // namespace string
 } // namespace data
