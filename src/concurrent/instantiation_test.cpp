b768767ae7b207983e287614622347f866ead49f
pepshiso <pepshiso9@gmail.com>
2012-09-03 06:07:46 +0900

    Revised instantiation tests.

--- src/concurrent/instantiation_test.cpp
+++ src/concurrent/instantiation_test.cpp
@@ -1,20 +1,23 @@
 #include "chan.h"
+#include "mvar.h"
+#include "pcbuf.h"
+#include "rwmutex.h"
 #include <string>
 
-template class pfi::concurrent::chan<int>;
-template class pfi::concurrent::chan<std::string>;
+namespace pfi {
+namespace concurrent {
 
-#include "mvar.h"
+template class chan<int>;
+template class chan<std::string>;
 
-template class pfi::concurrent::mvar<int>;
-template class pfi::concurrent::mvar<std::string>;
+template class mvar<int>;
+template class mvar<std::string>;
 
-#include "pcbuf.h"
+template class pcbuf<int>;
+template class pcbuf<std::string>;
 
-template class pfi::concurrent::pcbuf<int>;
-template class pfi::concurrent::pcbuf<std::string>;
+template class scoped_rwlock<pfi::concurrent::rlock_func>;
+template class scoped_rwlock<pfi::concurrent::wlock_func>;
 
-#include "rwmutex.h"
-
-template class pfi::concurrent::scoped_rwlock<pfi::concurrent::rlock_func>;
-template class pfi::concurrent::scoped_rwlock<pfi::concurrent::wlock_func>;
+} // namespace concurrent
+} // namespace pfi
--- src/data/instantiation_test.cpp
+++ src/data/instantiation_test.cpp
@@ -1,27 +1,26 @@
 #include "fenwick_tree.h"
-
-namespace data = pfi::data;
-
-template class data::fenwick_tree<int>;
-
-
 #include "string/kmp.h"
+#include "string/algorithm.h"
+#include "string/ustring.h"
 #include <string>
 
-namespace string = data::string;
+namespace pfi {
+namespace data {
 
-template class string::kmp<std::string>;
+template class fenwick_tree<int>;
 
+namespace string {
 
-#include "string/algorithm.h"
+template class kmp<std::string>;
 
-template int string::edit_distance<std::string>(const std::string&, const std::string&);
-template int string::longest_common_subsequence<std::string>(const std::string&, const std::string&);
-template int string::longest_increasing_subsequence<std::string>(const std::string&);
+template int edit_distance<std::string>(const std::string&, const std::string&);
+template int longest_common_subsequence<std::string>(const std::string&, const std::string&);
+template int longest_increasing_subsequence<std::string>(const std::string&);
 
+template ustring::ustring<char*>(char*, char*);
+template uchar chars_to_uchar<char*>(char*&);
+template void uchar_to_chars<char*>(uchar, char*&);
 
-#include "string/ustring.h"
-
-template string::ustring::ustring<char*>(char*, char*);
-template string::uchar pfi::data::string::chars_to_uchar<char*>(char*&);
-template void string::uchar_to_chars<char*>(uchar, char*&);
+} // namespace string
+} // namespace data
+} // namespace pfi
