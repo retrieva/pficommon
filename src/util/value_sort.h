0196bdf502c189e56f3991f520921219ec668d31
pepshiso <pepshiso9@gmail.com>
2012-09-04 18:09:47 +0900

    Changed names in util/value_sort.

--- src/util/value_sort.h
+++ src/util/value_sort.h
@@ -39,9 +39,9 @@
 namespace pfi {
 namespace util {
 
-template<typename _Key, typename _Value>
-  std::vector<std::pair<_Value,_Key> > value_sort(const std::vector<std::pair<_Key,_Value> >& vs, size_t num) {
-  std::vector<std::pair<_Value,_Key> > res;
+template<typename Key, typename Value>
+  std::vector<std::pair<Value,Key> > value_sort(const std::vector<std::pair<Key,Value> >& vs, size_t num) {
+  std::vector<std::pair<Value,Key> > res;
   for (size_t i=0;i<vs.size();++i) res.push_back(std::make_pair(vs[i].second,vs[i].first));
   num=std::min(num,vs.size());
   partial_sort(res.begin(),res.begin()+num,res.end());
