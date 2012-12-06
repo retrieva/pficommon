81c3684a3e3b643a86734513c568934824da443d
Nobuyuki Kubota <nobu.k.jp+github@gmail.com>
2012-12-06 18:51:35 +0900

    Improved test cases for ipv4_address

--- src/network/ipv4_test.cpp
+++ src/network/ipv4_test.cpp
@@ -35,62 +35,95 @@
 
 using pfi::network::ipv4_address;
 
-TEST(ipv4_address, constructor)
+TEST(ipv4_address, construct_none)
 {
-  {
-    ipv4_address ip;
-    ipv4_address ip0000(0, 0, 0, 0);
-    EXPECT_EQ(ip0000, ip);
-  }
-  {
-    ipv4_address ip_from_ints(128, 128, 128, 128);
-    ipv4_address ip_from_string("128.128.128.128");
-    EXPECT_EQ(ip_from_ints, ip_from_string);
-  }
+  ipv4_address ip;
+  ipv4_address ip0000(0, 0, 0, 0);
+  EXPECT_EQ(ip0000, ip);
 }
 
-TEST(ipv4_address, comparison)
+TEST(ipv4_address, construct_from_string)
 {
-  {
-    ipv4_address ip;
-    EXPECT_TRUE(ip == ip);
-    EXPECT_FALSE(ip != ip);
-    EXPECT_FALSE(ip < ip);
-  }
-  {
-    ipv4_address ip1(1, 1, 1, 1);
-    ipv4_address ip2(1, 1, 2, 0);
-    EXPECT_FALSE(ip1 == ip2);
-    EXPECT_TRUE(ip1 != ip2);
-    EXPECT_TRUE(ip1 < ip2);
-    EXPECT_FALSE(ip2 < ip1);
-  }
-  {
-    ipv4_address ip1(1, 1, 1, 1);
-    ipv4_address ip2(1, 2, 0, 1);
-    EXPECT_FALSE(ip1 == ip2);
-    EXPECT_TRUE(ip1 != ip2);
-    EXPECT_TRUE(ip1 < ip2);
-    EXPECT_FALSE(ip2 < ip1);
-  }
-  {
-    ipv4_address ip1(1, 1, 1, 1);
-    ipv4_address ip2(2, 0, 1, 1);
-    EXPECT_FALSE(ip1 == ip2);
-    EXPECT_TRUE(ip1 != ip2);
-    EXPECT_TRUE(ip1 < ip2);
-    EXPECT_FALSE(ip2 < ip1);
-  }
+  ipv4_address ip_from_ints(128, 128, 128, 128);
+  ipv4_address ip_from_string("128.128.128.128");
+  EXPECT_EQ(ip_from_ints, ip_from_string);
+}
+
+TEST(ipv4_address, compare_to_self)
+{
+  ipv4_address ip;
+  EXPECT_TRUE(ip == ip);
+  EXPECT_FALSE(ip != ip);
+  EXPECT_FALSE(ip < ip);
+}
+
+TEST(ipv4_address, compare_pos_3)
+{
+  ipv4_address ip1(1, 1, 1, 1);
+  ipv4_address ip2(1, 1, 1, 2);
+  EXPECT_FALSE(ip1 == ip2);
+  EXPECT_TRUE(ip1 != ip2);
+  EXPECT_TRUE(ip1 < ip2);
+  EXPECT_FALSE(ip2 < ip1);
+}
+
+TEST(ipv4_address, compare_pos_2)
+{
+  ipv4_address ip1(1, 1, 1, 1);
+  ipv4_address ip2(1, 1, 2, 0);
+  EXPECT_FALSE(ip1 == ip2);
+  EXPECT_TRUE(ip1 != ip2);
+  EXPECT_TRUE(ip1 < ip2);
+  EXPECT_FALSE(ip2 < ip1);
+}
+
+TEST(ipv4_address, compare_pos_1)
+{
+  ipv4_address ip1(1, 1, 1, 1);
+  ipv4_address ip2(1, 2, 0, 1);
+  EXPECT_FALSE(ip1 == ip2);
+  EXPECT_TRUE(ip1 != ip2);
+  EXPECT_TRUE(ip1 < ip2);
+  EXPECT_FALSE(ip2 < ip1);
+}
+
+TEST(ipv4_address, compare_pos_0)
+{
+  ipv4_address ip1(1, 1, 1, 1);
+  ipv4_address ip2(2, 0, 1, 1);
+  EXPECT_FALSE(ip1 == ip2);
+  EXPECT_TRUE(ip1 != ip2);
+  EXPECT_TRUE(ip1 < ip2);
+  EXPECT_FALSE(ip2 < ip1);
+}
+
+TEST(ipv4_address, to_string_none)
+{
+  ipv4_address ip;
+  EXPECT_EQ("0.0.0.0", ip.to_string());
 }
 
 TEST(ipv4_address, to_string)
 {
-  {
-    ipv4_address ip;
-    EXPECT_EQ("0.0.0.0", ip.to_string());
-  }
-  {
-    ipv4_address ip(123, 123, 123, 123);
-    EXPECT_EQ("123.123.123.123", ip.to_string());
-  }
+  ipv4_address ip(123, 123, 123, 123);
+  EXPECT_EQ("123.123.123.123", ip.to_string());
+}
+
+class ipv4_invalid : public testing::TestWithParam<std::string> {
+};
+
+INSTANTIATE_TEST_CASE_P(ipv4_invalid_instance,
+                        ipv4_invalid,
+                        testing::Values("192", "192.", "192.168", "192.168.", "192.168.0", "192.168.0.",
+                                        "1192.168.0.1", "192.1168.0.1", "192.168.1000.1", "192.168.0.1111",
+                                        "256.0.0.0", "0.256.0.0", "0.0.256.0", "0.0.0.256",
+                                        "-1.0.0.0", "0.-1.0.0", "0.0.-1.0", "0.0.0.-1",
+                                        "192.168,0.1"));
+
+// TODO: "192.168.0.1." should be invalid
+// TODO: tests for octal numbers
+
+TEST_P(ipv4_invalid, test)
+{
+  EXPECT_EQ(ipv4_address::none, ipv4_address(GetParam()));
 }
